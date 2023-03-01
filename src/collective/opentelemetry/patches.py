20a358e3f7bd3f27f6f452b71415975128db9e1b
David Glick <david@glicksoftware.com>
2023-02-28 16:33:06 -0800

    Add spans for collective.solr, fixes #9

--- src/collective/opentelemetry/patches.py
+++ src/collective/opentelemetry/patches.py
@@ -6,12 +6,14 @@ def apply_patches():
 
     # image scale generation
     from plone.namedfile import scaling
+
     scaling.scaleImage = tracer.start_as_current_span(
         "scaleImage [plone.namedfile.scaling]"
     )(scaling.scaleImage)
 
-    # catalog indexing
+    # ZCatalog indexing
     from Products.ZCatalog.Catalog import Catalog
+
     orig_catalogObject = Catalog.catalogObject
     orig_search = Catalog.search
 
@@ -24,15 +26,15 @@ def apply_patches():
 
     Catalog.catalogObject = trace_catalogObject
 
-    # catalog queries
-    def trace_search(self, query, sort_index=None, reverse=False, limit=None, merge=True):
+    # ZCatalog queries
+    def trace_search(
+        self, query, sort_index=None, reverse=False, limit=None, merge=True
+    ):
         if isinstance(sort_index, list):
             sort_indexes = [index.id for index in sort_index]
         else:
             sort_indexes = sort_index.id if sort_index is not None else "None"
-        with tracer.start_as_current_span(
-            "search [Products.ZCatalog.Catalog]"
-        ) as span:
+        with tracer.start_as_current_span("search [Products.ZCatalog.Catalog]") as span:
             span.set_attribute("plone.zcatalog.query", str(query))
             span.set_attribute("plone.zcatalog.sort_indexes", sort_indexes)
             span.set_attribute("plone.zcatalog.reverse", reverse)
@@ -40,3 +42,34 @@ def apply_patches():
             return orig_search(self, query, sort_index, reverse, limit, merge)
 
     Catalog.search = trace_search
+
+    try:
+        import collective.solr
+    except ImportError:
+        pass
+    else:
+        # collective.solr indexing
+        from collective.solr.indexer import SolrIndexProcessor
+
+        orig_index = SolrIndexProcessor.index
+
+        def trace_index(self, obj, attributes=None):
+            with tracer.start_as_current_span(
+                "index [collective.solr.indexer]"
+            ) as span:
+                span.set_attribute("plone.solr.path", "/".join(obj.getPhysicalPath()))
+                return orig_index(self, obj, attributes)
+
+        SolrIndexProcessor.index = trace_index
+
+        # collective.solr queries
+        from collective.solr.solr import SolrConnection
+
+        orig_solr_search = SolrConnection.search
+
+        def trace_solr_search(self, **params):
+            with tracer.start_as_current_span("search [collective.solr.solr]") as span:
+                span.set_attribute("plone.solr.params", str(params))
+                return orig_solr_search(self, **params)
+
+        SolrConnection.search = trace_solr_search
